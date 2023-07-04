63fddc1c8afcbebda9cd665698a8e4a8d89d704f
Piotr Migaj <piotr.migaj@capgemini.com>
2023-07-04 11:30:38 +0200

    00-start-with-templates

--- demand/application/src/main/java/hexmatcher/demand/application/port/ExistsDemandByIdPort.java
+++
@@ -1,8 +0,0 @@
-package hexmatcher.demand.application.port;
-
-import hexmatcher.demand.domain.valueobject.DemandId;
-
-@FunctionalInterface
-public interface ExistsDemandByIdPort {
-    boolean existsById(DemandId demandId);
-}
