b0ae3f9c3ec79e05c46ef3f4b2f2999fac48997f
Piotr Migaj <piotr.migaj@capgemini.com>
2023-07-04 11:57:09 +0200

    03-accept-candidate-use-case

---
+++ demand/application/src/main/java/hexmatcher/demand/application/usecase/AcceptCandidateUseCase.java
@@ -0,0 +1,34 @@
+package hexmatcher.demand.application.usecase;
+
+import hexmatcher.demand.application.port.QueryDemandByIdPort;
+import hexmatcher.demand.domain.entity.Demand;
+import hexmatcher.demand.domain.valueobject.CandidateId;
+import hexmatcher.demand.domain.valueobject.DemandId;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@RequiredArgsConstructor
+@Validated
+public class AcceptCandidateUseCase {
+
+    private static final String DEMAND_WITH_ID_NOT_FOUND = "Demand with ID='%s' not found.";
+    private final QueryDemandByIdPort queryDemandByIdPort;
+
+    @Transactional
+    public void handle(@Valid AcceptCandidateCommand acceptCandidateCommand){
+        DemandId demandId = acceptCandidateCommand.demandId();
+        Demand demand = queryDemandByIdPort.queryBy(demandId)
+                .orElseThrow(()->new EntityNotFoundException(DEMAND_WITH_ID_NOT_FOUND.formatted(demandId.value())));
+        demand.acceptCandidate(acceptCandidateCommand.candidateId());
+    }
+
+    public record AcceptCandidateCommand(@NotNull DemandId demandId, @NotNull CandidateId candidateId) {
+    }
+
+}
